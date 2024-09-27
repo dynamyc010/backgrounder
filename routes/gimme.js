2b52485027171a309e1ff9c6a88e9072e631162d
dynamyc <github@dynamyc.me>
2024-09-27 12:25:29 +0200

    🎇 Initial Upload

---
+++ routes/gimme.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+const path = require('path');
+const fs = require('fs');
+
+/* GET random image */
+router.get('/', function(req, res, next) {
+    
+    const options = {
+        root: path.join(__dirname, '../', 'backgrounds')
+    }
+
+    fs.readdir("./backgrounds/", (err, files) => {
+        if(!err){
+            file = files[(Math.floor(Math.random() * files.length))];
+            // console.log(file)
+            res.type(file.includes(".png") ? "image/png" : "image/jpeg");
+            res.sendFile(file,options, function (err){
+                if(err){
+                    res.status(500);
+                    // console.log("Error: ", err);
+                }
+                else{
+                    // console.log("Image sent.");
+                }
+            })
+        }else{
+            res.status(500);
+        }
+    });
+});
+
+module.exports = router;
